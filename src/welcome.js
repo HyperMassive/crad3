4f6d7191933bc932af2d57fad242bca230b7dfa5
Hyper Massive <hypermassive1@gmail.com>
2018-03-13 21:29:44 +0000

    added some homepage spiel.

--- src/welcome.js
+++ src/welcome.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Typography } from 'material-ui';
 
-const Welcome = () => <div>Welcome</div>;
+const Welcome = () => (
+  <div>
+    <Typography variant="display3">Home</Typography>
+    <Typography variant="subheading">reconciling react and d3</Typography>
+    <Typography variant="body1">
+      React and D3 are two frontend javascript libs which both rely on
+      monitoring and update the browser DOM, which means they cant be used
+      easily together.
+      <br />
+      <br />
+      // todo: flesh out and explain other differences.
+    </Typography>
+  </div>
+);
 
 export default Welcome;
