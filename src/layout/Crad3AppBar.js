2d2c47ab313037759a4372efdb25192ddb404cdd
Hyper Massive <hypermassive1@gmail.com>
2018-03-13 19:05:46 +0000

    add link to github page

--- src/layout/Crad3AppBar.js
+++ src/layout/Crad3AppBar.js
@@ -4,19 +4,26 @@ import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
 
 const styles = theme => ({
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
+  },
+  flex: {
+    flex: 1,
   },
 });
 
 const Crad3AppBar = ({ classes }) => (
   <AppBar position="absolute" className={classes.appBar}>
     <Toolbar>
-      <Typography variant="title" color="inherit" noWrap>
+      <Typography variant="title" color="inherit" className={classes.flex}>
         CRAD3
       </Typography>
+      <Button href="https://github.com/HyperMassive/crad3/" color="inherit">
+        Github
+      </Button>
     </Toolbar>
   </AppBar>
 );
